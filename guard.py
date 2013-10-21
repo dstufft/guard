33b7b05ba78ea6d59b389a3dec302189b9edf28c
Donald Stufft <donald@stufft.io>
2013-10-21 10:22:28 -0400

    Add some metadata about guard.py

--- guard.py
+++ guard.py
@@ -1,4 +1,26 @@
+# Copyright 2013 Donald Stufft
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import functools
+
+
+__title__ = "guard"
+__version__ = "1.0.dev0"
+__summary__ = "WSGI Middlewares for Web Application Security"
+__license__ = "Apache License v2.0"
+__uri__ = "https://github.com/dstufft/guard"
+__author__ = "Donald Stufft"
+__email__ = "donald@stufft.io"
 
 
 class ContentSecurityPolicy(object):
